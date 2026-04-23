e94b15f11e93926dd7b0e3abca01b8c658b1158f
AKTÜRK <enessyasar5@gmail.com>
2026-04-23 19:12:23 +0300

    initial commit

---
+++ src/Product.java
@@ -0,0 +1,30 @@
+public class Product {
+    private int id;
+    private double price;
+    private String name;
+    private String description;
+
+    public Product(int id, double fiyat, String bookName, String explain) {
+        this.id = id;
+        this.price =fiyat;
+        this.name=bookName;
+        this.description =explain;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
